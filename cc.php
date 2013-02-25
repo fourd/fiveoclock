f6ff5fdd7d6dfa3c26da6783a7aa741b6b610854
Matthew <9558109@gmail.com>
2013-02-25 03:30:48 +0000

    adding countrylist handler file

---
+++ cc.php
@@ -0,0 +1,12 @@
+<?php
+$cc_arr = array();
+/** turns the csv into an associative array where the 2 letter code is the key and the name is the value
+*
+*/
+if (($handle = fopen("cclist.csv", "r")) !== FALSE) {
+ while (($data = fgetcsv($handle, 0, ";")) !== FALSE) {
+  $cc_arr[$data[1]] = $data[0];
+ } 
+ fclose($handle);
+} 
+?>
